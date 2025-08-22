a2050ba65d454ff0d2810d827484385e0b72fabf
Nabin2060 <141759067+Nabin2060@users.noreply.github.com>
2025-08-22 08:29:39 +0545

    sort and foreach

---
+++ d_array/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace c_array;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] myArr = { 2, 4, 5, 6 };
+
+        //foreach loop
+        // foreach (var item in myArr)
+        // {
+        //      System.Console.WriteLine(item);
+        // }
+        // Console.WriteLine("Hello, World!");
+
+        // for loop
+        // for (int i = 0; i < 5; i++)
+        // for(int i = 0; i < myArr.Length; i ++)
+        // {
+        //     System.Console.WriteLine(myArr[i]);
+        // }
+
+        //sorting array
+        // Array.Sort(myArr);
+        // foreach (var item in myArr)
+        // {
+        //      System.Console.WriteLine(item);
+        // }
+
+        // char
+        // char[] myCharr = { 'h', 't', 's', 'w' };
+        // foreach (char item in myCharr)
+        // {
+        //     System.Console.WriteLine(item);
+        // }
+
+        //string
+        string[] myCharr = { "hrjr", "ty5y" };
+        foreach (string item in myCharr)
+        {
+            System.Console.WriteLine(item);
+        }
+    }
+}
