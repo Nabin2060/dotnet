70ac1aee85597002be263992e82cc7a85c27ff57
Nabin2060 <141759067+Nabin2060@users.noreply.github.com>
2025-08-22 08:28:48 +0545

    sort and foreach

---
+++ d_array/b_array_sort/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace b_array_sort;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] arr = [ 2,5, 3, 10, 54 ];
+        // Array.Sort(arr);
+        Array.Reverse(arr);
+
+        foreach (var item in arr)
+        {
+            System.Console.WriteLine(item);
+        } 
+
+    }
+}
