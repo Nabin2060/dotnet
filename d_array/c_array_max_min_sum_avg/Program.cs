81fccc36ce2f654599b6dd4099e2106c1dafeb91
Nabin2060 <141759067+Nabin2060@users.noreply.github.com>
2025-08-24 07:04:03 +0545

    array method

---
+++ d_array/c_array_max_min_sum_avg/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace c_array_max_min_sum_avg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 12, 44, 66, 77, 88, 876, 43 };
+
+            int min = arr[0];
+            int max = arr[0];
+            int sum = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] < min)
+                    min = arr[i];
+                if (arr[i] > max)
+                    max = arr[i];
+                sum += arr[i];
+            }
+
+            double avg = (double)sum / arr.Length;
+
+            Console.WriteLine("Minimum: " + min);
+            Console.WriteLine("Maximum: " + max);
+            Console.WriteLine("Sum: " + sum);
+            Console.WriteLine("Average: " + avg);
+        }
+    }
+}
