47420e55d6c37abe069bb3f16653051cd5e6bcac
Nabin2060 <141759067+Nabin2060@users.noreply.github.com>
2025-08-25 06:50:50 +0545

    implicit conversion

---
+++ type_Con/a_implicit/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace a_implicit;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        byte a = 10;
+        float b = a;
+        System.Console.WriteLine("The byte value before implicit :" + a); 
+        System.Console.WriteLine("The float value after implicit ::" + b); 
+
+    }
+}
