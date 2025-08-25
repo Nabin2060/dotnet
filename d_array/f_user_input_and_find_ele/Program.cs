6557eb8f50d51a375daa9559e4636adf9db64284
Nabin2060 <141759067+Nabin2060@users.noreply.github.com>
2025-08-25 06:49:04 +0545

    user input to check array

---
+++ d_array/f_user_input_and_find_ele/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace user_input_and_find_ele;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] myNum = { 1, 3, 5, 7 };
+        System.Console.WriteLine("Enter a number to find:");
+        // int input = Convert.ToInt32(System.Console.ReadLine());
+        int input = int.Parse(Console.ReadLine());
+        bool found = false;
+        foreach (var item in myNum)
+        {
+            if (item == input)
+            {
+                found = true;
+                break;
+            }
+        }
+        if (found)
+        {
+            System.Console.WriteLine("Number found...");
+        }
+        else
+        {
+            System.Console.WriteLine("Number not found...");
+        }
+    }
+}   
