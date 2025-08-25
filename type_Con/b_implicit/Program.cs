724cf930b3a1dd38f959f6af9cdef3fd0898d013
Nabin2060 <141759067+Nabin2060@users.noreply.github.com>
2025-08-25 06:51:23 +0545

    implicit narrowing eg

---
+++ type_Con/b_implicit/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace b_implicit;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // byte a = 256;
+        byte a = 255;
+
+        int b = a;
+        System.Console.WriteLine("The byte value before implicit :" + a);
+        System.Console.WriteLine("The int value after implicit ::" + b);
+    }
+}
