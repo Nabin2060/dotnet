01a105969fa00982e9cfb749496d6fd0c606593a
Nabin2060 <141759067+Nabin2060@users.noreply.github.com>
2025-08-25 06:35:27 +0545

    string

---
+++ d_array/d_string_sort/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace string_sort;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] myChar = { "ram", "hari", "ashok" };
+        int len = myChar.Length;
+        // sort 
+        // Array.Sort(myChar);
+
+        //
+        // reverse 
+        // Array.Reverse(myChar);
+        // foreach (var item in myChar)
+        // {
+        //     Console.WriteLine(item);
+        // }
+
+        // used for loop
+        for (int i = 0; i < len; i++)
+        {
+            System.Console.WriteLine("Name {0} is = {1}", i + 1, myChar[i]);
+        }
+        // Array.Sort(myChar);
+        Array.Reverse(myChar);
+        System.Console.WriteLine("After sorting:");
+            for (int i = 0; i < len; i++)
+            {
+                System.Console.WriteLine("Name {0} is = {1}", i + 1, myChar[i]);
+            }
+        
+    }
+}
