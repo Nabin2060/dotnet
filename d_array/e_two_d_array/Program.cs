720083b256848bbac72d859bc0fe89d7973aa37d
Nabin2060 <141759067+Nabin2060@users.noreply.github.com>
2025-08-25 06:39:08 +0545

    two dimi array eg

---
+++ d_array/e_two_d_array/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace two_d_array;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] myArr = { { 1, 3, 5, 7 }, { 5, 44, 6, 8 } };
+        System.Console.WriteLine(myArr[1, 2]);
+
+        // use foreach
+        foreach (var item in myArr)
+        {
+            System.Console.WriteLine(item);
+        }
+        
+    }
+}
