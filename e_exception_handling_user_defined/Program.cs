0460fac99b5059be61235cec89af34d90dfebe05
Nabin2060 <141759067+Nabin2060@users.noreply.github.com>
2025-11-04 08:41:40 +0545

    exception practice

---
+++ e_exception_handling_user_defined/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+// voter age check example
+
+class InvalidVoters : Exception
+{
+    public InvalidVoters(string str)
+    {
+        System.Console.WriteLine(str);
+    }
+}
+
+class Programs
+{
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("Enter your age :");
+        int age = int.Parse(Console.ReadLine());
+        try
+        {
+            if (age <18)
+            {
+                throw new InvalidVoters("You are not eligible to vote");
+            } else
+            {
+                Console.WriteLine("youre eligible to vote");
+            }
+        } catch (InvalidVoters err)
+        {
+            System.Console.WriteLine("Error found :" + err. Message);
+        }
+    }
+}
