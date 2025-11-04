3aeeb5167b5900200a09b438d8cfa8d825881bc9
Nabin2060 <141759067+Nabin2060@users.noreply.github.com>
2025-11-04 08:40:28 +0545

    virtual method

---
+++ f_virtual_Method/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class SuperClass
+{
+    public virtual int add(int a, int b)
+    {
+        return a + b;
+    }
+}
+class subClass : SuperClass
+{
+    public override int add(int a, int b)
+    {
+        return (a + b + 33);
+    }
+}
+
+class Programss
+{
+    static void Main(string[] args)
+    {
+        SuperClass obj = new subClass();
+        System.Console.WriteLine("The new redefined sum = " + obj.add(220,333));
+    }
+}
