c02856372e0d66f9f7d98e7e9c4b7e8f126e174d
Nabin2060 <141759067+Nabin2060@users.noreply.github.com>
2025-11-05 08:40:55 +0545

    abstract method

---
+++ g_abstract_method/a_abstract_method/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+abstract class HelloNp
+{
+    public abstract void hi();
+}
+class kathmandu: HelloNp
+{
+    public override void hi()
+    {
+        System.Console.WriteLine("Hello from Kathmandu");
+    }
+}
+
+class Bhakt: kathmandu
+{
+    public override void hi()
+    {
+        System.Console.WriteLine("Hello from Bhakt");
+    }
+}
+class Progras
+{
+    static void Main(string[] args)
+    {
+        HelloNp h;
+        h = new kathmandu();
+        h.hi();
+        h = new Bhakt();
+        h.hi();
+
+
+    }
+}
