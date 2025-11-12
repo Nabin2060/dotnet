66cbb33cba99cd81d5f8cf86f431096fff853f4e
Nabin2060 <141759067+Nabin2060@users.noreply.github.com>
2025-11-12 08:17:03 +0545

    delegate eg

---
+++ h_delegate/a_delegate/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+public delegate void MyDelegate(string name);
+
+class Progra
+{
+    public static void displayName(string name)
+    {
+        System.Console.WriteLine("Hello :  " + name);
+    }
+    static void Main(string[] args)
+    {
+        MyDelegate del = new MyDelegate(displayName);
+        del("Dotnet Delegate");
+    }
+}
